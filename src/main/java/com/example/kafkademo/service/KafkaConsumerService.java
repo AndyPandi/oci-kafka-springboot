ad0b2d2c77d9529646ef7ab10663b9af20d30452
seongil.jeong <seongil.jeong@oracle.com>
2025-10-15 14:58:25 +0900

    분리

--- src/main/java/com/example/kafkademo/service/KafkaConsumerService.java
+++ src/main/java/com/example/kafkademo/service/KafkaConsumerService.java
@@ -4,7 +4,7 @@ import com.example.kafkademo.config.KafkaConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +16,7 @@ public class KafkaConsumerService {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
-    // 임시로 비활성화 - DNS 문제 해결 후 활성화
-    // @KafkaListener(topics = KafkaConfig.TOPIC_NAME, groupId = "spring-group")
+    @KafkaListener(topics = KafkaConfig.TOPIC_NAME, groupId = "spring-group")
     public void consume(String message) {
         logger.info("Received message: {}", message);
         
